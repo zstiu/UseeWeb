e1edce638ce9ba04ffa832bb9b76e2f3906af7f9
zstiu <zstiu@foxmail.com>
2017-08-19 14:14:45 +0800

    1.fix full screen; 2.add three.js and test success

--- src/components/Danmu/Danmu.vue
+++ src/components/Danmu/Danmu.vue
@@ -1,22 +1,56 @@
 <template>
   <div class="danmu">
-    <h1>danmu</h1>
-    <canvas ref="danmuCanvas"/>
+    <canvas id="danmuCanvas"/>
   </div>
 </template>
 
 <script>
+import * as THREE from "three";
+
 export default {
   name: 'danmu',
-  mounted() {
-    let canvas = this.$refs;
-    canvas.danmuCanvas.height = window.screen.height;
-  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
-  }
+  },
+  mounted() {
+    var scene, camera;
+    var geometry, material, mesh;
+    let that = this;
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.z = 1000;
+
+    geometry = new THREE.BoxGeometry( 200, 200, 200 );
+    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+
+    let renderer = new THREE.WebGLRenderer({
+        canvas: document.getElementById('danmuCanvas')
+    });
+    console.log(renderer);
+    renderer.setClearColor();
+
+    function animate() {
+
+      requestAnimationFrame( animate );
+
+      mesh.rotation.x += 0.01;
+      mesh.rotation.y += 0.02;
+
+      renderer.render( scene, camera );
+
+    }
+    animate();
+    // canvas.danmuCanvas.height = window.screen.height;
+
+  },
 }
 </script>
 
