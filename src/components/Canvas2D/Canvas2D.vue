91558f86e6f4931e92cf35f3d621660e697cca00
zstiu <zstiu@foxmail.com>
2017-10-08 23:32:00 +0800

    修改danmu整合方式。TODO:随机速度与坐标值，碰撞检测

--- src/components/Canvas2D/Canvas2D.vue
+++ src/components/Canvas2D/Canvas2D.vue
@@ -21,17 +21,44 @@
 
       var ctx = document.getElementById('Canvas2D').getContext('2d');
 
-      class Danmu {
-        constructor(config, context){
-          this.x = config.x; // X坐标
-          this.y = config.y; // Y坐标
-          this.speedX = config.speedX; // 水平方向速度
-          this.speedY = config.speedY; // 垂直方向速度
-          this.context = context; // 获取context的引用
+      class DanmuBoard {
+        constructor(context){
+
+          this.danmu = [];
+
+          this.context = context;
+
+          this.draw();
+//          {
+//            text: config.text,
+//            x : config.x, // X坐标
+//            y : config.y, // Y坐标
+//            speedX : config.speedX, // 水平方向速度
+//            speedY : config.speedY, // 垂直方向速度
+//            context : context, // 获取context的引用
+//            width : context.measureText(config.text),
+//          };
         }
-        move() {
-          this.x += this.speedX;
-          this.y += this.speedY;
+
+        push(config){
+          this.danmu.push({
+            text: config.text,
+            x : config.x, // X坐标
+            y : config.y, // Y坐标
+            speedX : config.speedX, // 水平方向速度
+            speedY : config.speedY, // 垂直方向速度
+            width : this.context.measureText(config.text),
+            show : true,
+          })
+        }
+
+        move(value) {
+
+//          this.danmu.map(function(value, key){
+            value.x -= value.speedX;
+//          })
+
+//          this.y += this.speedY;
         };
 
         draw(){
@@ -42,9 +69,31 @@
           var context = this.context;
 
           context.font = "50px serif";
-          context.fillText("Hello world", this.x, this.y);
+
+          this.danmu.map(function(value, key){
+
+            if(value.show){
+              that.move(value);
+
+              context.fillText(value.text, value.x, value.y);
+              if (value.x <= 0){//-this.width+100){
+//                delete this;
+                value.show = false;
+                console.log("yeah");
+//                console.log(this);
+//            this.context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+                return;
+              }
+            }
+
+          })
+
+
           // 移动，更新位置
-          this.move();
+
+//          console.log(this.x);
+//          console.log(this.width);
+
           window.requestAnimationFrame(function() {that.draw()});
         };
 
@@ -53,19 +102,36 @@
       var text = ctx.measureText("foo");
       console.log(text);
 
-      let config = {
-        x: 10,
-        y: 10,
+      let config1 = {
+        text: "Hello world1",
+        x: window.innerWidth,
+        y: 100,
+        speedX: 1,
+        speedY: 1,
+      };
+      let config2 = {
+        text: "Hello world2",
+        x: window.innerWidth,
+        y: 200,
         speedX: 1,
         speedY: 1,
       }
 
-      var dan = new Danmu(config, ctx);
+      var dan = new DanmuBoard(ctx);
+//      new Danmu({
+//        text:"another",
+//        x: window.innerWidth,
+//        y: 300,
+//        speedX: 2,
+//        speedY: 2,
+//      }, ctx);
+
+      dan.push(config1);
+      dan.push(config2);
 
       console.log(dan);
 
-      dan.draw();
-
+//      dan.draw();
 
     },
   }
